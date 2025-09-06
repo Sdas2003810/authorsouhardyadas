e56db244db1d489b3d56d517fe1046b79f207a1f
Sdas2003810 <souhardyadasacademics@yahoo.com>
2025-09-07 01:06:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,12 +2,12 @@
 const heading = document.querySelector('h1');
 
 const button = document.createElement('button');
-button.textContent = 'Click Me!';
+button.textContent = 'Say Hello 👋';
 button.className = 'mt-4 px-4 py-2 bg-green-700 text-white rounded hover:bg-green-800 transition duration-300';
 
 document.querySelector('#home').appendChild(button);
 
 button.addEventListener('click', () => {
-  heading.textContent = 'Hello Interactive World!';
+  heading.textContent = 'Hello My Wattpad Readers!';
   button.textContent = 'Clicked!';
 });
